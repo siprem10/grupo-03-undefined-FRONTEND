fe6d646ff4af5cb0e10101c3ea21fd30df96ebd9
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-12-05 15:32:54 +0100

    feat: :sparkles: Layout component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
-import Header from "../Header/Header";
+import Layout from '../../Layout/Layout';
+import Header from '../Header/Header';
 
 export default function Home() {
-
-    return (
-        <>
-            <Header/>
-        </>
-    )
+  return (
+    <Layout>
+      <h1>Hola Alkemy</h1>
+    </Layout>
+  );
 }
