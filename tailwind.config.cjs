d8acfdb5c188a99a40712ad7954c5ed2a3a1aa8e
Franco D'Angelo <81258487+franodangelo@users.noreply.github.com>
2022-12-06 15:29:14 -0300

    Config/palette (#9)
    * react loader spinner and sweetalert2 installed
    
    * color palette
    
    * colors for success, info, alert and error status

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#004643",
+      secondary: "#abd1c6",
+      tertiary: "#e16162",
+      accent: "#f9bc60",
+      white: "#fffffe",
+      black: "#001e1d",
+      success: "#20FC8F",
+      info: "#2660A4",
+      alert: "#F0A202",
+      error: "#FF2E00"
+    },
     extend: {},
   },
   plugins: [],
