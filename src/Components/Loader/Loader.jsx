0bbe4b8f6368b9f020b9f1234e56ad7bd7aa1e6d
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-06 12:57:08 -0300

    preliminary loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useState } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 
 export default function Loader() {
+
+    // este estado debe estar en cada pagina donde vamos a usar el loader
+    const [loading, setLoading] = useState(false);
+
     return (
-        <div>Loader</div>
+        <BounceLoader
+            color="#36d7b7"
+            loading
+            size={120}
+            speedMultiplier={0.5}
+        />
     )
 }
