414af28690f705f803035cd1ba4f4ae21695caa5
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-12-08 17:46:01 +0100

    feat: :sparkles: Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/user/userSlice';
+import authReducer from '../features/auth/authSlice';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    user: userReducer,
+    auth: authReducer
+  }
 });
