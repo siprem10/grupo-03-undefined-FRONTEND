19ed4288d9ce128b235d702553fb6f0bdf97160a
siprem10 <siprem10@gmail.com>
2022-12-10 03:47:26 -0300

    comment(userAction)

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,22 +1,14 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { HttpService } from '../../Service/HttpService';
+// import { apiPublic } from '../../Service/HttpService';
 
-export const fetchUserInfo = createAsyncThunk('user/fetchUserInfo', async () => {
-  try {
-    const httpService = new HttpService();
-    const response = await httpService.apiPrivate().get('/auth/me');
-    const { id, firstName, lastName, email, avatar, roleId, points } = response.data.body;
-    const user = {
-      id,
-      firstName,
-      lastName,
-      email,
-      avatar,
-      roleId,
-      points
-    };
-    return user;
-  } catch (error) {
-    return error.message;
-  }
-});
+// export const register = () => async (dispatch) => {
+
+//   try {
+//     const response = await apiPublic.post('/users', { email, password, firstName, lastName });
+//     const user = response.data.body;
+//     console.log(user);
+
+//     return user;
+//   } catch (error) {
+//     return error.message;
+//   }
+// };
