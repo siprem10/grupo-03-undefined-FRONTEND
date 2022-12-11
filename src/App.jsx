ddc55bc42f4a9ca420a4162f7114c18e4339707e
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-12-11 04:53:33 +0100

    feat: :lipstick: Users list in Admin Panel

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,20 @@ import Home from './Components/Home/Home';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
 import { UserForm, createUserFields } from './Components/Forms/UserForms';
+import PanelAdmin from './Pages/PanelAdmin/PanelAdmin';
 
 function App() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route exact path='/register' element={<Register />} />
-                <Route exact path='/login' element={<Login />} />
-                <Route
-                    path='/create-user'
-                    element={<UserForm {...createUserFields} />}
-                />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route path="/panel-admin" element={<PanelAdmin />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route path="/create-user" element={<UserForm {...createUserFields} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
