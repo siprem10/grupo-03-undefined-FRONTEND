ff3783a29e2fd96c82910a27e5ffd077a5b4647c
siprem10 <siprem10@gmail.com>
2022-12-11 19:12:08 -0300

    feature(UI header auth status)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import SessionDropdown from '../SessionDropdown/SessionDropdown';
 import { BiHomeAlt } from 'react-icons/bi';
 import { TbHeartRateMonitor } from 'react-icons/tb';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
   const isLowRes = useMediaQuery({ maxWidth: 900 });
   const isVeryLowRes = useMediaQuery({ maxWidth: 400 });
   const path = useLocation().pathname;
+
+  const { status } = useSelector(state => state.auth);
 
   const sections = [
     { name: 'Inicio', to: '/', icon: BiHomeAlt },
@@ -17,27 +19,27 @@ export default function Header() {
 
   return (
     <nav
-      className={`p-4 flex flex-wrap w-full h-auto px-10 items-center shadow-lg bg-white text-primary ${
-        isVeryLowRes ? 'justify-center' : 'justify-between'
-      }`}>
+      className={`p-4 flex flex-wrap w-full h-auto px-10 items-center shadow-lg bg-white text-primary ${isVeryLowRes ? 'justify-center' : 'justify-between'
+        }`}>
       <h1 className="flex justify-center py-2 px-4 rounded-full border-2 bg-main border-emerald-900 md:text-2xl font-bold text-md tracking-wider ease-out duration-300">
         {isLowRes ? 'AW' : 'AlkyBank Wallet'}
       </h1>
-      <ul className="flex flex-wrap self-center">
-        {sections?.map((section, i) => (
-          <li
-            key={i}
-            className="p-3.5 flex md:text-xl items-center justify-center text-base font-bold uppercase text-gray-500 hover:text-tertiary ease-out duration-300">
-            {!isLowRes && <section.icon className="flex mr-2 md:w-6 w-5 h-auto"></section.icon>}
-            <Link
-              to={section.to}
-              className={path === section.to ? 'text-emerald-900 hover:text-tertiary' : ''}>
-              {section.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
+      {status === "success" &&
+        <ul className="flex flex-wrap self-center">
+          {sections?.map((section, i) => (
+            <li
+              key={i}
+              className="p-3.5 flex md:text-xl items-center justify-center text-base font-bold uppercase text-gray-500 hover:text-tertiary ease-out duration-300">
+              {!isLowRes && <section.icon className="flex mr-2 md:w-6 w-5 h-auto"></section.icon>}
+              <Link
+                to={section.to}
+                className={path === section.to ? 'text-emerald-900 hover:text-tertiary' : ''}>
+                {section.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      }
       <SessionDropdown />
     </nav>
   );
--- src/Components/SessionDropdown/SessionDropdown.jsx
+++ src/Components/SessionDropdown/SessionDropdown.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../redux/actions/authActions';
 import { setImgUserErr } from '../../Utils/ImgErr';
 
@@ -27,87 +27,82 @@ export default function SessionDropdown() {
 
   function handleShow(e) {
     if (!authenticated) {
-      return navigate('/login');
+      return navigate("/login");
     }
+
     setIsOpen(!isOpen);
     e.stopPropagation();
   }
 
   return (
     <div className="flex justify-center">
-      {status === 'loading' ? (
-        'loading...'
-      ) : (
-        <>
-          <button
-            onClick={handleShow}
-            id="dropdownDividerButton"
-            data-dropdown-toggle="dropdownDivider"
-            className="text-white bg-primary hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3.5 py-2.5 text-center inline-flex items-center dark:bg-primary dark:hover:bg-tertiary dark:focus:ring-blue-800"
-            type="button">
-            {authenticated ? (
-              <>
-                <img
-                  src={userData.avatar ?? 'err'}
-                  onError={e => setImgUserErr(e)}
-                  alt="img found"
-                  className="shadow rounded-full md:h-10 h-7 border-none mr-2 ease-out duration-300"
-                />
+      <button
+        onClick={handleShow}
+        id="dropdownDividerButton"
+        data-dropdown-toggle="dropdownDivider"
+        className="text-white bg-primary hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-3.5 py-2.5 text-center inline-flex items-center dark:bg-primary dark:hover:bg-tertiary dark:focus:ring-blue-800"
+        type="button">
 
-                <span>{userData.firstName ?? ''}</span>
-                <svg
-                  className="ml-2 w-4 h-4"
-                  aria-hidden="true"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M19 9l-7 7-7-7"></path>
-                </svg>
-              </>
-            ) : (
-              'Iniciar sesión'
-            )}
-          </button>
+        {authenticated ? (
+          <>
+            <img
+              src={userData.avatar ?? 'err'}
+              onError={e => setImgUserErr(e)}
+              alt="img found"
+              className="shadow rounded-full md:h-10 h-7 border-none mr-2 ease-out duration-300"
+            />
+            <span>{userData.firstName ?? ''}</span>
+            <svg
+              className="ml-2 w-4 h-4"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"></path>
+            </svg>
+          </>)
+          : (
+            <span className='md:p-1 p-0.5 ease-out duration-300'>Iniciar Sesión</span>
+          )
 
-          {authenticated && (
-            <div
-              style={{ top: '70px' }}
-              className={
-                !isOpen
-                  ? 'hidden'
-                  : 't-10 absolute border border-emerald-500 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'
-              }>
-              <ul
-                className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownDividerButton">
-                {items?.map((item, i) => (
-                  <li key={i}>
-                    {item.to && (
-                      <Link
-                        to={item.to}
-                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        {item.name}
-                      </Link>
-                    )}
-                    {item.onClick && (
-                      <p
-                        onClick={item.onClick}
-                        className="cursor-pointer block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        {item.name}
-                      </p>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </>
-      )}
-    </div>
+        }
+      </button>
+
+      <div
+        style={{ top: '70px' }}
+        className={
+          !isOpen
+            ? 'hidden'
+            : 't-10 absolute border border-emerald-500 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'
+        }>
+        <ul
+          className="py-1 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDividerButton">
+          {items?.map((item, i) => (
+            <li key={i}>
+              {item.to && (
+                <Link
+                  to={item.to}
+                  className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                  {item.name}
+                </Link>
+              )}
+              {item.onClick && (
+                <p
+                  onClick={item.onClick}
+                  className="cursor-pointer block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                  {item.name}
+                </p>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div >
   );
 }
