64193fdaaf5da7376ca7be1094a0332ef5b3b0c6
siprem10 <siprem10@gmail.com>
2022-12-11 04:01:29 -0300

    feature(protected route by role & auth)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
 import PanelAdmin from './Pages/PanelAdmin/PanelAdmin';
 import { CreateCategories, CreateTransactions, EditCategories, EditTransactions } from './Pages';
 import { UserForm, createUserFields } from './Components/Forms/UserForms';
+import { isLogged, isLoggedAdmin } from './Hooks/isSession';
 
 function App() {
+
+    const isSession = isLogged();
+    const isSessionAdmin = isLoggedAdmin();
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route exact path='/register' element={<Register />} />
-                <Route exact path='/login' element={<Login />} />
-                <Route path="/panel-admin" element={<PanelAdmin />} />
+                <Route path='/' element={isSession ? <Home /> : <Navigate to="/login" />} />
+                <Route exact path='/register' element={isSession ? <Navigate to="/" /> : <Register />} />
+                <Route exact path='/login' element={isSession ? <Navigate to="/" /> : <Login />} />
+                <Route path="/panel-admin" element={isSessionAdmin ? <PanelAdmin /> : <Navigate to="/" />} />
                 <Route
                     path='/create-user'
-                    element={<UserForm {...createUserFields} />}
+                    element={isSession ? <Navigate to="/" /> : <UserForm {...createUserFields} />}
                 />
-                <Route exact path='/categories/create' element={<CreateCategories />} />
-                <Route exact path='/categories/edit' element={<EditCategories />} />
-                <Route exact path='/transactions/create' element={<CreateTransactions />} />
-                <Route exact path='/transactions/edit' element={<EditTransactions />} />
+                <Route exact path='/categories/create' element={isSession ? <CreateCategories /> : <Navigate to="/login" />} />
+                <Route exact path='/categories/edit' element={isSession ? <EditCategories /> : <Navigate to="/login" />} />
+                <Route exact path='/transactions/create' element={isSession ? <CreateTransactions /> : <Navigate to="/login" />} />
+                <Route exact path='/transactions/edit' element={isSession ? <EditTransactions /> : <Navigate to="/login" /> } />
             </Routes>
         </BrowserRouter>
     );
