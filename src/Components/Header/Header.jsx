fea3820295adcf734fab29d31d8c756429d1fe41
siprem10 <siprem10@gmail.com>
2022-12-11 00:43:59 -0300

    refactor(components)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import BaseButton from '../BaseButton/BaseButton';
+import { Link, useLocation } from 'react-router-dom';
 import SessionDropdown from '../SessionDropdown/SessionDropdown';
 
 export default function Header() {
 
-    const sections = ['gastos', 'balance', 'movimientos'];
+    const sections = [
+        { name: "Inicio", to: "/" },        
+        { name: "Actividad", to: "/transactions" }
+    ];
+
+    const path = useLocation().pathname;
 
     return (
         <nav className='flex w-full h-20 px-10 items-center justify-between shadow-lg bg-white text-primary'>
             <h1 className='px-4 font-bold text-2xl tracking-wider'>Alkybank Wallet</h1>
             <ul className='flex gap-8'>
-                {sections?.map((section, i) =>                     
-                    <Link key={i} to={`/${section}`}>
-                        <li className='font-semibold uppercase text-slate-900 hover:text-tertiary ease-out duration-300'>{section}</li>
-                    </Link>
+                {sections?.map((section, i) =>
+                    <li key={i} className='text-2xl font-bold uppercase text-gray-500 hover:text-tertiary ease-out duration-300'>
+                        <Link to={section.to} className={path === section.to ? "text-emerald-900" : ""}>{section.name}</Link>
+                    </li>
                 )}
             </ul>
-            <div className='flex gap-4'>
-                <BaseButton text={"Enviar dinero"} color='secondaryButton'></BaseButton>
-                <BaseButton text={"Cargar saldo"} />
-            </div>
             <SessionDropdown></SessionDropdown>
         </nav>
     )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import Layout from '../Layout/Layout';
-import Header from '../Header/Header';
 
 export default function Home() {
   return (
