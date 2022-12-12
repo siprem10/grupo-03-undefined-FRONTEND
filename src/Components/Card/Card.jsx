78e171764c5cca44bcb0d480e499c01eb5d8a7a5
siprem10 <siprem10@gmail.com>
2022-12-12 03:55:34 -0300

    UI(extra padding in card.jsx)

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@
 export default function Card({className, children}) {
 
     return (
-        <div className={`md:w-1/2 2xl:w-2/6	w-96 py-2 px-8 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ${className ?? ""}`}>
+        <div className={`md:w-1/2 2xl:w-2/6	w-96 py-4 px-8 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ${className ?? ""}`}>
             {children}
         </div>
     )
