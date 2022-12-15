a0267e32d725e7f86a129a0454a97cb04af7e9d7
siprem10 <siprem10@gmail.com>
2022-12-15 02:58:07 -0300

    UI

--- src/Pages/PanelAdmin/PanelAdmin.jsx
+++ src/Pages/PanelAdmin/PanelAdmin.jsx
@@ -5,10 +5,12 @@ const PanelAdmin = () => {
   return (
     <Layout>
       <div className="w-full p-6">
-        <h1 className="text-3xl font-medium text-white my-4 text-center uppercase">Usuarios registrados</h1>
+        <h1 className="xs:text-xl md:text-2xl xl:text-3xl text-xs font-medium text-white my-4 text-center uppercase">Usuarios registrados</h1>
         <UsersTable />
       </div>
     </Layout>
   );
 };
 export default PanelAdmin;
+
+l
