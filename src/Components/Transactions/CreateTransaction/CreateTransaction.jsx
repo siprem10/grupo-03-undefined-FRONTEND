5a86f369ffef47202b066ebd7be54b4db3eb5f84
siprem10 <siprem10@gmail.com>
2022-12-16 00:09:06 -0300

    feature(responsive)

--- src/Components/Transactions/CreateTransaction/CreateTransaction.jsx
+++ src/Components/Transactions/CreateTransaction/CreateTransaction.jsx
@@ -20,7 +20,7 @@ export default function CreateTransaction() {
   const CARGA_DE_SALDO = "Carga de Saldo";
   const TRANSFERENCIA = "Transferencia";
   const PAGO_DE_SERVICIOS = "Pago de Servicios";
-  
+
   const enumLocation = {
     "deposit-money": CARGA_DE_SALDO,
     "transfer": TRANSFERENCIA,
@@ -162,7 +162,7 @@ export default function CreateTransaction() {
     try {
       const httpService = new HttpService();
       const request = await httpService.apiPrivate().post(`/transactions`, { ...inputState, toUserId: findUser.id });
-      
+
       if (request.data.body) {
         alertOk("Transacción generada correctamente!");
         setResetStates();
@@ -176,66 +176,65 @@ export default function CreateTransaction() {
 
   return (
     <Layout>
-      <div className="flex w-full justify-center">
-        <Card className="flex flex-col w-fit items-center justify-center">
-          <h1 className="mb-2 text-primary font-bold text-2xl uppercase">Generar {typeTransaction}</h1>
-          <form className="form w-full mt-1 mb-3" onSubmit={e => e.preventDefault()}>
-            <Dropdown setState={setTypeTransaction} itemDefSelect={typeTransaction} className="mb-4 w-full" title={"Tipo de transacción"} items={type}></Dropdown>
+      <div className="flex flex-col justify-center w-[90%] xs:w-[410px] md:w-[500px]">
+         <form className="form w-full mt-1 mb-3" onSubmit={e => e.preventDefault()}>
+         <h1 className="-ml-1 mb-3 text-primary font-bold text-md xs:text-xl uppercase">Generar {typeTransaction}</h1>
+       
+          <Dropdown setState={setTypeTransaction} itemDefSelect={typeTransaction} className="mb-4 w-full" title={"Tipo de transacción"} items={type}></Dropdown>
 
-            {typeTransaction === PAGO_DE_SERVICIOS &&
-              <CategoryDropdown stateSelect={categorySelect} setState={handleSetCategory} className="mb-4 w-full" title={"Servicio a pagar"} items={categories}></CategoryDropdown>
-            }
+          {typeTransaction === PAGO_DE_SERVICIOS &&
+            <CategoryDropdown stateSelect={categorySelect} setState={handleSetCategory} className="mb-4 w-full" title={"Servicio a pagar"} items={categories}></CategoryDropdown>
+          }
 
-            {typeTransaction === TRANSFERENCIA &&
-              <div className='flex items-center gap-2 mb-4'>
-                <div className="w-full">
-                  <label className="label">Destinatario</label>
-                  <input
-                    className="inputForm"
-                    type="text"
-                    placeholder="correo@mail.com"
-                    name="toUserId"
-                    value={inputState.toUserId}
-                    onChange={e => handleSetInput(e)}>
-                  </input>
-                  <div className="h-5">
-                    {inputError.toUserId && <p className="inputFormErr">{inputError.toUserId}</p>}
-                  </div>
+          {typeTransaction === TRANSFERENCIA &&
+            <div className='flex items-center gap-2 mb-4'>
+              <div className="w-full">
+                <label className="label">Destinatario</label>
+                <input
+                  className="inputForm"
+                  type="text"
+                  placeholder="correo@mail.com"
+                  name="toUserId"
+                  value={inputState.toUserId}
+                  onChange={e => handleSetInput(e)}>
+                </input>
+                <div className="h-5">
+                  {inputError.toUserId && <p className="inputFormErr">{inputError.toUserId}</p>}
                 </div>
-                <BaseButton disabled={isBtnFindDisabled()} className={`h-10 mt-2 ${isBtnFindDisabled() ? "disable" : ""}`} onClick={handleFindUser} text="Buscar" />
-              </div>}
-            <FindUser className={"mb-4 w-full"} />
-            <div className="mb-4">
-              <label className="label">Monto</label>
-              <input
-                className="inputForm"
-                type="number"
-                placeholder="Monto"
-                name="amount"
-                value={inputState.amount}
-                onChange={e => handleSetInput(e)}></input>
-              <div className="h-5">
-                {inputError.amount && <p className="inputFormErr">{inputError.amount}</p>}
               </div>
-
+              <BaseButton disabled={isBtnFindDisabled()} className={`h-10 mt-2 ${isBtnFindDisabled() ? "disable" : ""}`} onClick={handleFindUser} text="Buscar" />
+            </div>}
+          <FindUser className={"mb-4 w-full"} />
+          <div className="mb-4">
+            <label className="label">Monto</label>
+            <input
+              className="inputForm"
+              type="number"
+              placeholder="Monto"
+              name="amount"
+              value={inputState.amount}
+              onChange={e => handleSetInput(e)}></input>
+            <div className="h-5">
+              {inputError.amount && <p className="inputFormErr">{inputError.amount}</p>}
             </div>
 
-            <div className="mb-4">
-              <label className="label">Concepto</label>
-              <input
-                className="inputForm"
-                type="text"
-                placeholder="Concepto"
-                name="concept"
-                value={inputState.concept}
-                onChange={e => handleSetInput(e)}></input>
-              <div className="h-5">
-                {inputError.concept && <p className="inputFormErr">{inputError.concept}</p>}
-              </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="label">Concepto</label>
+            <input
+              className="inputForm"
+              type="text"
+              placeholder="Concepto"
+              name="concept"
+              value={inputState.concept}
+              onChange={e => handleSetInput(e)}></input>
+            <div className="h-5">
+              {inputError.concept && <p className="inputFormErr">{inputError.concept}</p>}
             </div>
-            <BaseButton disabled={isBtnSendDisabled()} className={`w-full ${isBtnSendDisabled() ? "disable" : ""}`} onClick={handleSend} text="Enviar" />
-          </form>
-        </Card>
+          </div>
+          <BaseButton disabled={isBtnSendDisabled()} className={`w-full ${isBtnSendDisabled() ? "disable" : ""}`} onClick={handleSend} text="Enviar" />
+        </form>
       </div>
     </Layout>
   );
